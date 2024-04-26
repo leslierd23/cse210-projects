67d6705a82614df4ddc35aeb467de8512bbf8e9f
leslierd23 <leslie.lrodri@gmail.com>
2024-04-26 00:49:58 -0500

    Fixed conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
-}
+        Console.Write("What is your grade percentage? " );
+        string gradeUser = Console.ReadLine();
+        int gradePercent = int.Parse(gradeUser);
+
+        string letterGrade = "";
+
+        if (gradePercent >= 90 )
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercent > 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercent > 60)
+        {
+            letterGrade ="D";
+        }
+        else if (gradePercent < 60){
+            letterGrade ="F";
+        }
+    
+        
+        Console.WriteLine($"Your Grade percentage is {letterGrade}.");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congrats, you passed!");
+        }
+        else{
+            Console.WriteLine("I'm sorry, bette luck next time!");
+        }
+
+        
+      }  }

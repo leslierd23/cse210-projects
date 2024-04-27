cbe2ed490e35794181c1e04b106742e430c224f7
leslierd23 <leslie.lrodri@gmail.com>
2024-04-27 00:45:14 -0500

    Fixed loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?" );
+        
+        //int numberM = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int numberM = randomGenerator.Next(1,20);
+
+        int guessNumber= -1;
+
+        while (guessNumber != numberM )
+        {
+            Console.Write ("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (numberM > guessNumber)
+            {
+                Console.Write("Try Higher");
+            }
+            else if (numberM < guessNumber)
+            {
+                Console.WriteLine("Try lower please");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
     }
 }
